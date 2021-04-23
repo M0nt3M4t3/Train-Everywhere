c90497f2e791236c43b5846cb886b84d5818d1a3
Cassandrawindahl <cassandrawindahl@outlook.com>
2021-04-23 17:03:00 +0200

    En massa
    Implementerat så man kan skriva recensioner, flyttat alla statements till helpers så vi numera inte har så mycket i varje route.
    
    Co-Authored-By: kroonfilip <81237654+kroonfilip@users.noreply.github.com>

--- projektarbete/project.py
+++ projektarbete/project.py
@@ -1,58 +1,54 @@
 # coding: utf-8
 # Author: <>
 
-from bottle import route, run, template, request, static_file
-import os, sys
-import pyodbc as db
+from bottle import route, run, template, request, static_file, redirect 
 import helpers
 import safety
-
-server = 'localhost'
-username = safety.FB[0]
-password = safety.FB[1]
-database = 'Train_Everywhere'
-connection = db.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' +
-database + ';UID=' + username + ';PWD=' + password)
-cursor = connection.cursor() # type: db.Cursor
 
 @route("/")
 def start_page(): 
     ''' Startsidan på hemsidan. ''' 
-    return template("startsida")
+    return template("index")
 
-@route("/utegym")
-def list_utegym():
+@route("/gym")
+def list_gym():
     ''' Sidan som visar upp alla utegym som punktlista, funktionen tar alla 
     utegyms namn som finns i vår databastabell "utegym" och returnar detta 
     som en lista till HTML dokumentet utegym.html.
     '''
-    cursor.execute("SELECT Namn, Bild FROM Utegym ORDER BY Namn ASC")
-    gyms = cursor.fetchall()
-    return template("utegym", gyms=gyms)
 
-@route('/utegym/<pagename>')
-def show_utegym(pagename):
+    gyms = helpers.call_gyms()
+    return template("gym", gyms=gyms)
+
+@route("/gym/<pagename>")
+def show_gym(pagename):
     ''' Sidan för alla utegym. Funktionen tar pagename och hämtar ut 
     beskrivning av databastabellen "utegym" för det utegym som det står
     i pagenamet att det är. Hämtar också ut alla recensioner ur 
     databastabellen "recensioner" på samma viss.
     '''
-    cursor.execute ("""SELECT Beskrivning FROM Utegym WHERE Namn=?""", pagename)
-    gym_info = cursor.fetchall()
-    cursor.execute ("""
-                    SELECT R.Namn, R.Betyg, R.Datum, R.Kommentar
-                    FROM  Recensioner R JOIN Utegym U
-                    ON R.Gym_id = U.Gym_id
-                    WHERE U.Namn = ?
-                    ORDER BY R.Datum;""", pagename)
-    reviews = cursor.fetchall()
+    gym_info = helpers.call_info(pagename)
     text = gym_info[0][0]
     text = helpers.nl2br(text)
-    return template("show_utegym", pagename=pagename, gym_info=text, reviews=reviews)
+
+    reviews = helpers.call_reviews(pagename) 
+
+    return template("show_gym", pagename=pagename, gym_info=text, reviews=reviews)
+
+@route("/add_review", method="POST")
+def update_review():
+    '''
+    '''
+    pagename = request.forms.get("gym")
+    name = request.forms.get("name")
+    rating = request.forms.get("vol") 
+    comment = request.forms.get("comment")
+    helpers.add_review(name, rating, comment, pagename)
+
+    return redirect("/gym/" +pagename) 
 
 @route("/static/<filename>")
 def static_files(filename):
     return static_file(filename, root="static")
 
 run(host='localhost', port=8000, debug=True, reloader=True)#("hit med HTTP-metoden POST")
-
