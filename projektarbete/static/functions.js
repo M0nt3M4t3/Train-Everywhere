b28eaa9e54205a0f0a46d2fc7216e9e77cf66355
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2021-06-01 09:48:40 +0200

    Uppdaterat sökruta

--- projektarbete/static/functions.js
+++ projektarbete/static/functions.js
@@ -533,6 +533,7 @@ function getInfo(name, description, picture) {
     console.log(info)
     return info 
 } 
+
 // Sökfunktion
 function search_gym() {
     let input = document.getElementById('searchbar').value
@@ -560,27 +561,26 @@ ScrollToTop.addEventListener("click", function (){
     });
 });
 
+
+// Popup-funktion
 function popup_review() {
     alert("Tack för din recension!")
 }
 
 // Filtreringsknapp
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function dropdownFilterForAverage() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
